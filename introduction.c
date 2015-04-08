81a05b79bfad86445886e07f48e47ce8adb23398
DenisProkopenko <prokopenko@live.com>
2015-04-08 00:35:15 -0500

    Create introduction.c

---
+++ introduction.c
@@ -0,0 +1,12 @@
+int introduction();
+
+int introduction()
+{
+    printf("Welcome to the Craps Table\n");
+    printf("Rules:\n");
+    printf("1. Roll 2-dice\n");
+    printf("2. Win if the sum of 2-dice is a 7 or 11\n");
+    printf("3. Lose if the sum is 2, 3, or 12\n\n");
+    
+    return 0;
+    }
